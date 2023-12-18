b2e95832543ba8c6bef4a6528d0855405a222270
BimaPL <bimapramudya26@gmail.com>
2023-12-18 13:41:05 +0700

    Screen/Login

--- src/main/java/com/mycompany/pemilu/WelcomeFrame.java
+++ src/main/java/com/mycompany/pemilu/WelcomeFrame.java
@@ -1,11 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package com.mycompany.pemilu;
 
 /**
- *
  * @author user
  */
 public class WelcomeFrame extends javax.swing.JFrame {
