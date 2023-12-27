4fa78ef434aeee74ad9c8bcd1302e225b26dd66d
CraftTex <nadinezuhdan@gmail.com>
2023-12-27 17:23:26 +0700

    added calon model

---
+++ src/main/java/com/mycompany/pemilu/Model/Calon.java
@@ -0,0 +1,17 @@
+package com.mycompany.pemilu.Model;
+
+public class Calon {
+    private int id;
+
+    public Calon(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
